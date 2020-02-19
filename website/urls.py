410b77d1a3844b8691a07599d2e5f8d248f0de56
satyajeet <satyajeet@getvital.in>
2020-02-19 14:51:05 +0530

    pushing with params filtering where params is foregin key

--- website/urls.py
+++ website/urls.py
@@ -2,4 +2,5 @@ from django.urls import path,include,re_path
 from .import views
 urlpatterns =[
     re_path(r'^location/$',views.RegionListApiView.as_view(),name="RegionListApiView"),
+    re_path(r'^location/(?P<country>\w+)/$',views.CountryListApiView.as_view(),name="CountryListApiView"),
 ]
--- website/views.py
+++ website/views.py
@@ -9,3 +9,14 @@ from .models import *
 class RegionListApiView(ListAPIView):
     queryset=Region.objects.all()
     serializer_class=RegionSerializer
+    
+class CountryListApiView(ListAPIView):
+    queryset=Country.objects.all()
+    serializer_class=CountrySerializer
+    
+    def get_queryset(self):
+        country_var=self.kwargs.get('country',None)
+        queryset=self.queryset.filter(region__name=country_var) #look at region in country model and then name in Region model 
+        return queryset
+        
+        
